e3bfe19857e840e4f63f5e36cd790e9b91fe41ae
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-19 12:35:59 +0530

    initial commit

---
+++ Day 01/02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter the number a: ";
+    cin >> a;
+
+    cout << "Enter the number b: ";
+    cin >> b;
+
+    if (a>b){
+      cout << "A is greater" << endl;
+    }
+    else{
+      cout <<  "B is greater" << endl;
+    }
+}
