fa6f77f3eab854b98959a3652e45aabb52e118ab
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-20 11:22:56 +0530

    initial commit

---
+++ Day 02/05.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num;
+    bool isPrime = true;
+
+    cout << "Enter the number: ";
+    cin >> num;
+
+    if (num <= 1){
+        isPrime = false; 
+    }
+    else{
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime){
+        cout << num << " :is a Prime number." << endl;
+    } 
+    else{
+        cout << num << " :is a Non-Prime number." << endl;
+    }
+
+    return 0;
+}
