34c92759f6a77f744ec1849cb512c1fdf3e7b66e
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-23 00:50:16 +0530

    initial commit

---
+++ Day 01/exm.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//       int a = 9;
+
+//       if (a == 9){
+//             cout << "9" << endl;
+//       }
+//       else if (a>0){
+//             cout << "Positive" << endl;
+//       }
+//       else{
+//             cout << "Negative" << endl;
+//       }
+// }
+
