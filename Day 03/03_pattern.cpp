d2f1d307720febf6d3715c9677a1b9ac8c3f0705
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-23 01:50:23 +0530

    initial commit

---
+++ Day 03/03_pattern.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int n;
+      cout << "Enter the number: ";
+      cin >> n;
+
+      int i = 1;
+
+      while(i<=n){
+            int j = 1;
+            while(j<=n){
+                  cout << j;
+                  j++;
+            }
+            cout << endl;
+            i++;
+      }
+      return 0;
+}
+
+// Output:
+// 1234
+// 1234
+// 1234
+// 1234
