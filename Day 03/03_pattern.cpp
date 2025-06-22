d81f9e5db811ceb0b636e705fdc934e923323f06
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-23 01:56:03 +0530

    initial commit

--- Day 03/03_pattern.cpp
+++ Day 03/03_pattern.cpp
@@ -25,3 +25,30 @@ int main(){
 // 1234
 // 1234
 // 1234
+
+// Reverse number Patterns
+
+int main(){
+      int n;
+      cout << "Enter the number: ";
+      cin >> n;
+
+      int i = 1;
+
+      while(i<=n){
+            int j = 1;
+            while(j<=n){
+                  cout << n-j+1;
+                  j++;
+            }
+            cout << endl;
+            i++;
+      }
+      return 0;
+}
+
+// Output:
+// 4321
+// 4321
+// 4321
+// 4321
