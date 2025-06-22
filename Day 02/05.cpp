2a926f4644a58661023ab21c98bcb114c869e247
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-23 01:11:45 +0530

    initial commit

--- Day 02/05.cpp
+++ Day 02/05.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-    int num;
-    bool isPrime = true;
+// int main (){
+//     int num;
+//     bool isPrime = true;
 
+//     cout << "Enter the number: ";
+//     cin >> num;
+
+//     if (num <= 1){
+//         isPrime = false; 
+//     }
+//     else{
+//         for (int i = 2; i * i <= num; ++i) {
+//             if (num % i == 0) {
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//     }
+
+//     if (isPrime){
+//         cout << num << " :is a Prime number." << endl;
+//     } 
+//     else{
+//         cout << num << " :is a Non-Prime number." << endl;
+//     }
+
+//     return 0;
+// }
+
+// 2nd method
+
+int main(){
+    int n,  i = 2;
     cout << "Enter the number: ";
-    cin >> num;
+    cin >> n;
 
-    if (num <= 1){
-        isPrime = false; 
-    }
-    else{
-        for (int i = 2; i * i <= num; ++i) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
+    while(i<n){
+        if(n%i==0){
+            cout << "No Prime" << i << endl;
         }
+        else{
+            cout << "Prime number" << i << endl;
+        }
+        i = i + 1;
     }
-
-    if (isPrime){
-        cout << num << " :is a Prime number." << endl;
-    } 
-    else{
-        cout << num << " :is a Non-Prime number." << endl;
-    }
-
-    return 0;
 }
