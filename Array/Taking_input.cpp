c2142fdf9089725ae02d522486d51b09171158fa
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-09 11:33:48 +0530

    Array

---
+++ Array/Taking_input.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int arr[5];
+      cout << "Enter the value of n, (n = number of values to take input for): ";
+
+      int n;
+      cin >> n;
+
+      for(int i = 0; i<n; i++){
+            cout  << "Taking intput for indexs: " << i << endl;
+            cin >> arr[i];
+      }
+      cout << "Printing the values of array" << endl;
+      for(int i = 0; i<n; i++){
+            cout << arr[i] << ",";
+      }
+      cout << endl;
+}
+
+// Taking input of char
+
+int main(){
+      char arr[5];
+      cout << "Enter the value of n, (n = number of values to take input for): ";
+      
+      int n;
+      cin >> n;
+
+      for(int i = 0; i<n; i++){
+            cout << "Taking intput for indexs: " << i << endl;
+            cin >> arr[i];
+      }
+
+      cout << "Printing the array----" << endl;
+      for(int i = 0; i<n; i++){
+            cout << arr[i] << ", ";
+
+      }
+      cout << endl;
+}
