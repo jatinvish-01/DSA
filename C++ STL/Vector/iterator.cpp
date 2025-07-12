cc392b6cc86d9031863c00e322d60159da00b6df
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-12 22:13:03 +0530

    initial commit

---
+++ C++ STL/Vector/iterator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+      
+      vector<int> first;
+
+      first.push_back(10);
+      first.push_back(11);
+      first.push_back(12);
+      first.push_back(13);
+
+
+      vector<int> :: iterator it = first.begin();
+
+      while(it != first.end()){
+            cout << *it << " ";
+            it++;
+      }
+}     
