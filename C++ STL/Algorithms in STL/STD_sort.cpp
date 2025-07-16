2c061994b637cc8c9675b0dd28e9ef92e5333109
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-16 12:40:51 +0530

    initial commit

---
+++ C++ STL/Algorithms in STL/STD_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void printDouble(int a){
+      cout << 2*a << " ";
+}
+
+int main(){
+
+      vector<int> arr(5);
+
+      arr[0] = 11;
+      arr[1] = 17;
+      arr[2] = 35;
+      arr[3] = 4;
+      arr[4] = 50;
+
+
+      // int target = 11;
+      sort(arr.begin(), arr.end());
+
+      for(int a : arr){ // a Belongs to arr
+            cout << a << " ";
+      }
+      
+
+      return 0;
+}
