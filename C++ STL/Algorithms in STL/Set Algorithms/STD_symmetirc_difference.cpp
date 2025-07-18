37b67f82eda00d860f42102b9846673ded360426
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-18 11:06:19 +0530

    initial commit

---
+++ C++ STL/Algorithms in STL/Set Algorithms/STD_symmetirc_difference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+      std::vector<int> first{1,2,3,4};
+      std::vector<int> second{3,4,5,6};
+
+      std::vector<int> result;
+
+      std::set_symmetric_difference(first.begin(), first.end(), second.begin(), second.end(), inserter(result, result.begin()));
+
+      for(int a : result){
+            cout << a << " ";
+      }cout << endl;
+
+      return 0;
+}
