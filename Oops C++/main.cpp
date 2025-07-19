e53a97ced8ed00eb2d981b96bf3566d0bf568828
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-19 11:41:16 +0530

    initial commit

--- Oops C++/main.cpp
+++ Oops C++/main.cpp
@@ -12,12 +12,13 @@ class Student{
             }
             
             // Parameteriesed constructor
-            Student(int id, int age, string name, int nos){
+            Student(int id, int age, string name, int nos, float gpa){
                   cout << "Student Parameteriesed constructor called" << endl;
                   this->id = id;     
                   this->age = age;     
                   this->name = name;     
                   this->nos = nos;     
+                  this->gpa = new int (gpa);     
             }
 
             // Copy Constructor
@@ -30,7 +31,7 @@ class Student{
             }
 
             // Attributes / instance
-            int id, age, nos;
+            int id, age, nos, *gpa;
             string name;
 
             // Behaviours / Methods / Functions
@@ -53,38 +54,48 @@ class Student{
 int main(){
       // 1st method to print
 
-      Student a;
-      a.id = 1;
-      a.age = 18;
-      a.name = "Jatin";
-      a.nos = 6;
+      // Student a;
+      // a.id = 1;
+      // a.age = 18;
+      // a.name = "Jatin";
+      // a.nos = 6;
 
-      Student b;
-      b.id = 2;
-      b.age = 15;
-      b.name = "rahul";
-      b.nos = 4;
+      // Student b;
+      // b.id = 2;
+      // b.age = 15;
+      // b.name = "rahul";
+      // b.nos = 4;
 
-      a.study();
-      b.bunk();
+      // a.study();
+      // b.bunk();
 
       // 1st method to print
 
-      Student a(1, 15, "Ramish", 5);
-      Student b(2, 14, "Rahul", 5);
-      // Student c(3, 17, "aman", 5);
-      // Student d(4, 15, "sohal", 5);
+      // Student a(1, 15, "Ramish", 5); // stack memory
+      // Student b(2, 14, "Rahul", 5);
+      // // Student c(3, 17, "aman", 5);
+      // // Student d(4, 15, "sohal", 5);
 
-      cout << a.id << " " << a.name << " " << endl;
+      // // cout << a.id << " " << a.name << " " << endl;
 
-      a.sleep();
-      b.bunk();
+      // a.sleep();
+      // b.bunk();
 
-      // Copy Constructor
-      Student c = (a);
-      c.study();
+      // // Copy Constructor
+      // Student c = (a);
+      // c.study();
 
-      cout << c.name << " " << a.name << " " << endl;
+      // // cout << c.name << " " << a.name << " " << endl;
+
+      // Dynamic allocation, or student pointer
+
+      Student *a = new Student(1, 25, "Vishal", 7, 9.5); // Pointer - they point student object present in heap
+
+      cout << a->name << endl;
+      cout << a->age << endl;
+      a->study();
+      a->sleep();
+      delete a; // In dynamic memory location - we delect the destructor by it self
 
       return 0;
 }
